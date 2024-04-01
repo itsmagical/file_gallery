f0bcfe3a55c36704f51bd041623729655db0d1e9
itsmagical <Itsmagical163>
2024-04-01 14:08:20 +0800

    导出选择图片接口

--- lib/file_gallery.dart
+++ lib/file_gallery.dart
@@ -1,7 +1,10 @@
 library file_gallery;
 
 
-export '';
+export 'util/compress_util.dart';
+export 'util/file_type_util.dart';
+export 'package:image_picker/image_picker.dart';
+export 'package:wechat_assets_picker/wechat_assets_picker.dart';
 
 /// A Calculator.
 class Calculator {
